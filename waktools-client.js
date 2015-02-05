8493e6ca08ab9e1324bf240f96c233a4ce9f3601
unknown <eust@PC-1275.gv.li>
2015-02-05 14:32:22 +0100

    initial

---
+++ waktools-client.js
@@ -0,0 +1,354 @@
+﻿
+/**
+ * @author stefan eugster
+ */
+
+var WAKTOOLS = WAKTOOLS || {};
+
+
+/**
+ * match
+ */
+ 
+WAKTOOLS.match = function(regex, string) {
+ 	// extract amount
+	if ( string && string.match(regex) && string.match(regex).length > 1) {
+		return string.match(regex)[1]
+	} else {
+		return '';
+	}
+};
+
+
+/**
+ * convert nl into space
+ */
+
+WAKTOOLS.nl2space = function( str ) {
+    var str = str || '';
+    // replace line breaks with br tags
+    str = str.replace(/\n/g, ', ');
+    return str;
+};
+
+
+/**
+ * convert nl into <br />
+ */
+
+WAKTOOLS.nl2br = function( str ) {
+    var str = str || '';
+    // replace line breaks with br tags
+    str = str.replace(/\n/g, '<br />');
+    return str;
+};
+
+
+/**
+ * delay throttle
+ */
+
+WAKTOOLS.delay = (function(){
+  var timer = 0;
+  return function(callback, ms) {
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+
+/**
+ * error handler
+ */
+
+
+WAKTOOLS.Error = {};
+
+
+/**
+ * extract error message from datastore method
+ * ex. used for dropzone messages
+ *
+ * @param  {Object} datastore return value
+ * @return {String} error message
+ */
+ 
+WAKTOOLS.Error.message = function (event) {
+    
+    var message = '';
+
+    // get error message
+    if (event.result.errorMessage) {
+        message = event.result.errorMessage;
+    } else if (event.result.messages && event.result.messages.length > 0) {
+        message = event.result.messages.join('\n\n');
+    }
+    
+    return message;
+};
+
+
+/**
+ * screen handler
+ */
+
+WAKTOOLS.screen = function(path) {
+	// remove component
+	$$('mainScreen').removeComponent();
+	// start spinner
+	WAKTOOLS.Spinner.spin();
+	$$('mainScreen').loadComponent({
+		path: path,
+		onSuccess: function(event){
+			// stop active spinner
+			WAKTOOLS.Spinner.stop();
+		}
+	});
+};
+
+
+/**
+ * user login handler
+ */
+
+WAKTOOLS.login = function() {
+	if ( WAF.directory.currentUser() === null ) {
+		$('#mainScreen').addClass('show-login-dialog');
+		WAKTOOLS.screen('/components/public-login.waComponent');
+	} else {
+		// idle timer (logout after 10 min inactivity)
+		new WAKTOOLS.Idle(1000 * 60 * 10, WAKTOOLS.logout);
+		// add login name to header bar
+		$('#loginName').text(WAF.directory.currentUser().fullName);
+		// load user navigation bar
+		ds.Component.getNavigation({
+			onSuccess: function (event) {
+				$('#mainScreen').removeClass('show-login-dialog');
+				// display navigation
+				WAKTOOLS.navigation(event.result);
+				// load default login component
+				if ( WAF.directory.currentUserBelongsTo('Admin') ) {
+					WAKTOOLS.screen('/components/admin-settings.waComponent');
+				} else {
+					WAKTOOLS.screen('/components/public-page.waComponent');
+				}
+			}
+		});
+	}
+};
+
+
+/**
+ * user logout handler
+ */
+
+WAKTOOLS.logout = function() {
+	WAF.directory.logout({
+		onSuccess: function() {
+			window.location.reload();
+		}
+	});
+};
+
+
+/**
+ * user navigation
+ */
+
+WAKTOOLS.navigation = function ( data ) {
+	var $container = $('#navContainer'),
+		templateSource = $('#navigationTemplate').html(),
+		templateFn = Handlebars.compile(templateSource);
+	
+	$container.html( templateFn( data ) );
+	if ( data ) {
+		$('.mainNavItem:first').addClass('activeItem');
+	}	
+	// navigation cklick event
+	$container.on('click', '.mainNavItem', function(event) {
+		var $this = $(this);
+		var linkName = $this.text();
+
+		$('.mainNavItem').removeClass('activeItem');
+		// remove toast container
+		$('#toast-container').remove();
+		$this.addClass('activeItem');
+		WAKTOOLS.screen($this.attr('data-component'));
+	});
+	// navigation mouseenter event
+	$container.on('mouseenter', '.mainNavItem', function(event) {
+		$(this).addClass('hoverItem');
+	});
+	// navigation mouseleave event
+	$container.on('mouseleave', '.mainNavItem', function(event) {
+		$(this).removeClass('hoverItem');
+	});	
+};
+
+
+/**
+ * user notifications
+ */
+
+WAKTOOLS.alert = function(message, type) {	
+	try {
+	    var type = type || 'success';
+		// display message for debugging issues
+		console.log(message);	    
+	    // toastr default settings
+		toastr.options = {
+			closeButton: false,
+			timeOut: '5000',
+  			extendedTimeOut: '1000',
+  			positionClass: 'toast-bottom-right'
+		};
+		// check if message is string
+		if (Object.prototype.toString.call(message) === '[object String]' || Object.prototype.toString.call(message) === '[object Boolean]') {
+		    // display message
+			toastr[type]('' + message);		
+		} else if (Object.prototype.toString.call(message) === '[object Array]') {
+		    // loop array
+		    for (var i = 0; i < message.length; i++) {
+			    WAKTOOLS.alert(message[i], type);
+    		};
+		} else {
+			if ('result' in message) {
+				WAKTOOLS.alert(message.result);
+			} else if ('message' in message) {
+				WAKTOOLS.alert(message.message, type);
+			} else if ('error' in message) {
+				WAKTOOLS.alert(message.error, 'error');
+			} else if ('successMessage' in message) {
+				WAKTOOLS.alert(message.successMessage);
+			} else if ('errorMessage' in message ) {
+				WAKTOOLS.alert(message.errorMessage, 'error');
+			} else {
+				throw new Error('Unknown message object');
+			}
+		}
+	} catch (e) {
+		toastr.error('Es ist ein Fehler aufgetreten. Bitte schliessen sie die Anwendung und versuchen Sie es erneut.');
+		console.log(e);
+	}
+};
+
+
+/**
+ * modal component dialog
+ */
+
+WAKTOOLS.modal = function(options) {
+	try {
+		var options = options || {},
+			width = options.width || 452,
+			height = options.height || 250,
+			componentPath = options.componentPath || '/components/modal-confirm.waComponent',
+			dialog,
+			dialogComponent;
+
+		// dialog size
+		switch(options.size) {
+			case 'medium':
+				width = 1024;
+				height = 720;
+				break;
+			case 'full':
+				width = window.innerWidth - 80;
+				height = window.innerHeight - 80;
+				break;
+		}
+		// component title
+		waf.widgets.txtTitle.setValue(options.title);
+		dialog = waf.widgets.modalDialog;
+		dialogComponent = waf.widgets.modalDialogComponent;
+		// remove component
+		dialogComponent.removeComponent();
+		// load dialog component
+		dialogComponent.loadComponent({
+			path: componentPath,
+			userData: { 
+				onConfirm: options.onConfirm,
+				onCancel: options.onCancel,
+				message: options.message,
+				value: options.value,
+				selectedElementID: options.selectedElementID
+			}
+		});
+		// dialog size and position
+		dialog.setWidth(width);
+		dialog.setHeight(height);
+		dialog.setLeft((window.innerWidth - width)/2);
+		dialog.setBottom((window.innerHeight - height)/2);
+		dialog.displayDialog();		
+	} catch (e) {
+		console.log(e);
+	}	
+}
+
+
+/**
+ * spinner
+ */
+
+WAKTOOLS.Spinner = WAKTOOLS.Spinner || {};
+
+WAKTOOLS.Spinner.spin = function () {
+	if (this.spinner == null) {
+		this.spinner = new Spinner({
+			lines:  15, // The number of lines to draw
+			length: 15, // The length of each line
+			width:  3, // The line thickness
+			radius: 15, // The radius of the inner circle
+			color:  '#000000', // #rbg or #rrggbb
+			speed:  1, // Rounds per second
+			trail:  10, // Afterglow percentage
+			shadow: false // Whether to render a shadow
+		});
+		this.spinner.spin(document.getElementById('wrapperMain'));
+	}
+};
+
+WAKTOOLS.Spinner.stop = function () {
+	if (this.spinner) {
+		this.spinner.stop();
+		this.spinner = null;
+	}
+};
+
+
+/**
+ * idle timer
+ * @param  {Number}   time in milliseconds
+ * @param  {Function} timeout functions
+ */
+
+WAKTOOLS.Idle = function(time, timeoutFn) {
+	var _this = this;
+		
+	// idle time
+	_this.idleTime = time;
+	// timeout function
+	_this.timeoutFn = timeoutFn;
+	// add event listeners
+    window.addEventListener("mousemove", function(){_this.reset()}, false);
+    window.addEventListener("mousedown", function(){_this.reset()}, false);
+    window.addEventListener("keypress", function(){_this.reset()}, false);
+};
+
+WAKTOOLS.Idle.prototype.startTimer = function() {
+	var _this = this;
+	
+    // set timout function
+    _this.timeoutID = window.setTimeout(function(){
+    	_this.timeoutFn();
+    }, _this.idleTime);
+}
+
+WAKTOOLS.Idle.prototype.reset = function() {
+	var _this = this;
+
+	// reset timout
+    window.clearTimeout(_this.timeoutID);
+    // restart timer
+    _this.startTimer();		
+}
---
+++ waktools-server.js
@@ -0,0 +1,419 @@
+﻿
+/**
+ * @author stefan eugster
+ */
+
+WAKTOOLS = {};
+
+
+/**
+ * trim string endings
+ */
+
+WAKTOOLS.trim = function(str) {
+    // remove white spaces
+    var str = str || '';
+    
+    str = str.replace(/^\s+/g, '').replace(/\s+$/g, '');
+    return str;   
+};
+
+
+/**
+ * basic auth
+ */
+
+WAKTOOLS.basicAuth = function(user, password) {
+    var credentials = user + ':' + password,
+        credentials_hash,
+        basic_auth;
+       
+    // base64 hash
+    credentials_hash = new Buffer(credentials).toString('base64');
+    // basic auth string
+    basic_auth = "Basic " + credentials_hash;
+    
+    return basic_auth;
+};
+
+
+/**
+ * create thumbnail from image
+ *
+ * @param source (String)  source image file path
+ * @param target (String)  target image file path
+ * @param size   (Number)  max image size (length or heigth) in pixel
+ * @param crop   (Boolean) crop image to specified size if true
+ * @return result {Object} imagemagick result object
+ */
+
+WAKTOOLS.thumbnail = function (source, target, size, crop) {
+    try {
+        var im = require('image-magick'),
+            imCommand = '',
+            size = size || __CONFIG.IMAGE_SIZE_THUMBNAIL,
+            result;
+
+        // command
+        if (crop) {
+            imCommand = '"' + source + '" -thumbnail ' + size + 'x' + size + '^> -gravity center -crop ' + size + 'x' + size + ' +0+0 +repage "jpg:' + target + '"';    
+        } else {
+            imCommand = '"' + source + '" -thumbnail ' + size + 'x' + size + '> "jpg:' + target + '"';
+        }
+        // convert image
+        result = im.convert(imCommand);
+        // error log
+//        if (result.console && result.console.stdErr) {
+//            console.error('[IMAGE-MAGICK] ' + result.console.stdErr);     
+//        }
+
+        return result;
+    } catch (e) {
+       WAKTOOLS.log(e);
+       return e;
+    }
+}
+
+
+/**
+ * create thumbnail from pdf
+ *
+ * @param source (String)  source pdf file path
+ * @param target (String)  target image file path
+ * @param size   (Number)  max image size (length or heigth) in pixel
+ * @return result {Object} imagemagick result object
+ */
+
+WAKTOOLS.thumbnailPDF = function (source, target, size) {
+    try {
+        var im = require('image-magick'),
+            imCommand = '',
+            size = size || __CONFIG.IMAGE_SIZE_THUMBNAIL,
+            result;
+
+        // command
+        imCommand = '"' + source + '[0]" -density 150 -thumbnail ' + size + 'x' + size + '> -flatten "jpg:' + target + '"';
+        //imCommand = '"' + source + '[0]" -density 150 -thumbnail ' + size + 'x' + size + ' -background white -alpha remove "jpg:' + target + '"';
+        // convert pdf
+        result = im.convert(imCommand);
+        // error log
+//        if (result.console && result.console.stdErr) {
+//            console.error('[IMAGE-MAGICK] ' + result.console.stdErr);     
+//        }
+
+        return result;
+    } catch (e) {
+       WAKTOOLS.log(e);
+       return e;
+    }
+}
+
+
+/**
+ * convert image size
+ *
+ * @param source  {String} source image file path
+ * @param target  {String} target image file path
+ * @param size    {Number} max image size (length or heigth) in pixel
+ * @return result {Object} imagemagick result object
+ */
+
+WAKTOOLS.resize = function(source, target, size) {
+    try {
+        var im = require('image-magick'),
+            imCommand = '',
+            size = size || __CONFIG.IMAGE_SIZE_LARGE,
+            result;
+
+        // command     
+        imCommand = '"' + source + '" -resize ' + size + 'x' + size + '\> "' + target + '"';
+        // convert image
+        result = im.convert(imCommand);
+        // error log
+        if (result.console && result.console.stdErr) {
+            console.error('[IMAGE-MAGICK] ' + result.console.stdErr);     
+        }
+             
+       return result;
+    } catch (e) {
+       WAKTOOLS.log(e);
+       return e;     
+    }
+}
+
+
+/**
+ * load file icon
+ *
+ * @param dropzoneFile {Object} dropzone file object
+ * @return image       {Image}  thumbnail image object
+ */
+
+WAKTOOLS.icon = function(dropzoneFile) {
+    try {
+        var orgFile = File(ds.getDataFolder().path + 'tmp/' + dropzoneFile.name),
+            tmpFile = File(ds.getDataFolder().path + 'tmp/tmp_' + generateUUID()),
+            result;
+
+        // check if image
+        if(['image/jpeg', 'image/gif', 'image/png', 'image/bmp'].indexOf(dropzoneFile.type) !== -1) {
+            // create thumbnail
+            WAKTOOLS.thumbnail(orgFile.path, tmpFile.path);    
+            result = loadImage(tmpFile);  
+            // remove thumb file
+            tmpFile.remove();
+        } else if (['application/pdf'].indexOf(dropzoneFile.type) !== -1) {
+            // create thumbnail
+            WAKTOOLS.thumbnailPDF(orgFile.path, tmpFile.path);    
+            result = loadImage(tmpFile);  
+            // remove thumb file
+            tmpFile.remove();          
+        }else {
+            var icon = 'default.png';
+
+            if(['doc', 'docx'].indexOf(orgFile.extension) !== -1) {
+                icon = 'word.png';
+            } else if (['xls', 'xlsx'].indexOf(orgFile.extension) !== -1) {
+                icon = 'excel.png';
+            } else if (['ppt', 'pptx', 'pps', 'ppsx'].indexOf(orgFile.extension) !== -1) {
+                icon = 'powerpoint.png';
+            } else if (['wmv', 'mov', 'avi', 'mp3', 'mp4', 'm4a', 'm4v'].indexOf(orgFile.extension) !== -1) {
+                icon = 'media.png';
+            } else if (['txt', 'csv', 'xml'].indexOf(orgFile.extension) !== -1) {
+                icon = 'document.png';
+            }
+            result = loadImage(ds.getModelFolder().path + 'Libs/waktools/icons/' + icon);
+        }
+       
+        return result;
+    } catch (e) {
+        WAKTOOLS.log(e);
+        return e;     
+    }
+}
+
+
+/**
+ * sync web folder
+ */
+
+WAKTOOLS.sync = function() {
+    try {
+        if (os.isLinux) {
+            var worker = new SystemWorker(__CONFIG.SCRIPT_WWW_SYNC);
+         
+            // terminate worker after sincronisation
+            worker.onterminated = function () {        
+                exitWait(); 
+            };
+            // terminate system worker after 10 seconds if not responding
+            setTimeout(function() {
+                worker.terminate();
+            }, 10000);
+            wait();
+             
+            return true;
+        }     
+    } catch (e) {
+        WAKTOOLS.log(e);
+        return e; 
+    }
+};
+
+
+/**
+ * convert string to slug
+ */
+
+WAKTOOLS.convertToSlug = function(str) {
+    try {
+        str = str.replace(/^\s+|\s+$/g, ''); // trim
+        str = str.toLowerCase();
+
+        // remove accents, swap ñ for n, etc
+        var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+        var to   = "aaaaeeeeiiiioooouuuunc------";
+        for (var i=0, l=from.length ; i<l ; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+        }
+
+        str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+        return str;      
+    } catch (e) {
+        WAKTOOLS.log(e);
+        return e; 
+    }
+};
+
+
+/**
+ * error handler
+ */
+
+WAKTOOLS.log = function(error) {
+    try {
+        // log error
+        console.error('[SOURCE] ' + error.sourceURL + ' [LINE] ' + error.line + ' [MESSAGE] ' + error.message);
+       
+        return;     
+    } catch (e) {
+        console.error('WAKTOOLS.log() ' + e);
+    }
+};
+
+
+/**
+ * get path
+ */
+
+WAKTOOLS.path = function(entity, relationAttribute, pathAttribute) {
+    try {
+        if (entity[relationAttribute] && entity[relationAttribute][relationAttribute] == null) {
+            return entity[pathAttribute];
+        } else {
+            return WAKTOOLS.path(entity[relationAttribute], relationAttribute, pathAttribute) + '/' + entity[pathAttribute];
+        }       
+    } catch (e) {
+        WAKTOOLS.log(e);
+        return e; 
+    }
+};
+
+
+/**
+ * new line to <br>
+ */
+
+WAKTOOLS.nl2br = function(str) {
+    var str = WAKTOOLS.trim(str);
+    // replace line breaks with br tags
+    str = str.replace(/\n/g, '<br>');
+    
+    return str;
+};
+
+
+/**
+ * convert string to HTML char code
+ */
+
+WAKTOOLS.toHtmlCharCode = function(str) {
+    var resultArr = [];
+    for ( var i = 0; i < str.length; ++i ) {
+      resultArr.push('&#' + str.charCodeAt(i) + ';');
+    }
+    
+    return resultArr.join('');
+}
+
+
+/**
+ * trim string endings
+ */
+
+WAKTOOLS.teaserArray = function(datasource, orderByStr, maxItems) {
+    
+    var resultCollection = datasource.createEntityCollection();
+    var orgCollection = datasource.all().orderBy(orderByStr);
+    var orderByStr = orderByStr || 'ID';   
+    
+    for(var i = 0; i < maxItems && i < orgCollection.length; i++) {
+       resultCollection.add(orgCollection[i]);
+    }
+    
+    var resultArray = resultCollection.orderBy(orderByStr).toArray('');
+    
+    // free memory
+    orgCollection = null;
+    resultCollection = null;
+       
+    return resultArray;    
+};
+
+
+/**
+ * clone wakanda dataclass entity
+ */
+
+WAKTOOLS.cloneEntity = function (entity) {
+    var dataClass = entity.getDataClass(),
+        clone = dataClass.createEntity();   
+    
+    // clone entity
+    for (var i in entity) {
+        if (entity.hasOwnProperty(i)) {
+            if ((i != 'ID' && dataClass[i].kind == 'storage') || (dataClass[i].kind == 'relatedEntity')) {
+                clone[i] = entity[i];
+            }
+        }
+    }
+    // save new entity
+    clone.save();
+    
+    return clone;
+};
+
+
+/**
+ * backup datastore to backup folder
+ */
+
+WAKTOOLS.backup = function() {
+    try {
+       var backupFolder = Folder(__CONFIG.BACKUP_DESTINATION),
+         config,
+         options,
+         manifest,
+         report = '';
+       
+       // create folder if not existing       
+       if (! backupFolder.exists) {
+         backupFolder.create();
+       }
+       // backup config
+       var config = {
+           useUniqueNames: true,
+           maxRetainedBackups: __CONFIG.BACKUP_MAX_RETAINED_BACKUPS,
+           destination: backupFolder
+       };
+       // callback functions
+       var callback = {
+         addProblem: function(problem) {
+          // problem report
+             report = report + '[LEVEL] ' + problem.ErrorLevel + ' [NUMBER] ' + problem.ErrorNumber + ' [TEXT] ' + problem.ErrorText + '\n';
+           }
+       };
+       // start backup
+       try {
+         manifest = ds.backup(config, callback);   
+       } catch (e) {
+         WAKTOOLS.Error.log(e);
+       }
+        // return error if result is null
+        if ( manifest ) {
+           // infomail
+           require('email').sendMail({
+             subject: application.name + ' Backup erfolgreich [' + __CONFIG.SERVER_NAME + ']', 
+             content: moment().format('DD.MM.YYYY HH:mm') + ' - ' + application.name + ' Backup erstellt\n\n' + JSON.stringify(manifest, null, '\t'),
+             priority: '1'
+           });
+           
+           return {successMessage: 'Backup erstellt'};
+        } else {
+           // infomail
+           require('email').sendMail({
+             subject: application.name + ' Backup fehlgeschlagen [' + __CONFIG.SERVER_NAME + ']',
+             content: moment().format('DD.MM.YYYY HH:mm') + ' - ' + application.name + ' Backup auf ' + application.httpServer.ipAddress + ' ist fehlgeschlagen!\n\n' + report,
+             priority: '1'
+           });
+           
+           return {errorMessage: 'Backup fehlgeschlagen: ' + report};
+        }
+    } catch (e) {
+       WAKTOOLS.log(e);
+       return e;
+    }
+};
